45a154bb3435576ec4a1cf6dd2e213171520c4c9
T.V Raman <tv.raman.tv@gmail.com>
2020-06-18 10:10:21 -0700

    bound game size

--- pickup.c
+++ pickup.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 20
 
 int main (int argC, char  **argV) {
   printf("How many sticks would you like to play with?\n");
   int n;
   scanf("%d", &n);
   printf("Playing with %d sticks.\n", n);
-  int* fibs;
+  int fibs[MAX_SIZE];
   fibs[0] = 1;
-fibs[1] = 2;
- int index = 1;
- while (fibs [index -1] < n) {
-   fibs[index] = fibs[index -1] + fibs[index -2];
-   index++;
+  fibs[1] = 2;
+  int index = 1;
+  while (fibs [index -1] < n) {
+    fibs[index] = fibs[index -1] + fibs[index -2];
+    if (index == MAX_SIZE){
+      printf("You'll get bored with such a large %d\n", n);
+      exit(0);
+    }
+    index++;
+  }
+
+  int limit = n - 1;
+  int fib_base = fibs[0];
+  int fib_p = 0;
+  
+  for (int i = 0; i<MAX_SIZE; i++ ){
+    if (fibs[i] == n)   {
+      fib_p = 1;
+      break;
 }
+  }
+
+  if (fib_p != 1){
+    printf("I play first.\n");
+}
+
 }
