bf41298d1fa9b4dda8d1e1046a087710a206d0d1
T.V Raman <tv.raman.tv@gmail.com>
2020-06-18 10:56:06 -0700

    first turn

--- pickup.c
+++ pickup.c
@@ -28,11 +28,33 @@ int main (int argC, char  **argV) {
     if (fibs[i] == n)   {
       fib_p = 1;
       break;
-}
+    }
   }
 
   if (fib_p != 1){
     printf("I play first.\n");
-}
 
+    for (int i = 0; i<MAX_SIZE; i++ ){ /*  update fib_base */ 
+      if (fibs[i] < n)   {
+        fib_base = fibs[i];
+      } else {
+        break;
+      }
+    }
+
+    int k = n -fib_base;
+    /*  check for 3k <n rule */ 
+    while (3*k >= n) {
+      /*  Update fib_base */
+      for (int i = 0; i<MAX_SIZE; i++ ){ /*  update fib_base */ 
+        if (fibs[i] < k)   {
+          fib_base = fibs[i];
+        } else {
+          break;
+        }
+      }
+      printf("Move: %d\n", k - fib_base);
+    }
+
+  }
 }
