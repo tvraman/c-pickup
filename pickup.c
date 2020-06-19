83a654960bdeb52d320982540ac94fb209becb69
T.V Raman <tv.raman.tv@gmail.com>
2020-06-19 13:46:05 -0700

    *** empty log message ***

--- pickup.c
+++ pickup.c
@@ -29,8 +29,8 @@ int next_move(const int fibs[], int n, int limit) {
 }
 int main() {
   printf("Pick number of sticks (>2) \n");
-  int n;
-  int k;
+  uint n;
+  uint k;
   scanf("%d", &n);
   printf("Playing with %d sticks.\n", n);
   int fibs[MAX_SIZE];
